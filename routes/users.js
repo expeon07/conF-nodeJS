b81c0c3181b7da38caa14ae82703c0ec94ece161
Sheena Fernandez <sheena.fernandez78@gmail.com>
2020-05-06 21:20:34 +0900

    Passport JWT

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 var express = require('express');
 const bodyParser = require('body-parser');
 var User = require('../models/user');
+var passport = require('passport');
+var authenticate = require('../authenticate');
 
 var router = express.Router();
 router.use(bodyParser.json());
@@ -12,73 +14,31 @@ router.get('/', function(req, res, next) {
 
 router.post('/signup', (req, res, next) => {
   // check if username exists
-  User.findOne({ username: req.body.username})
-  .then((user) => {
-    if(user != null) {
-      var err = new Error('User  ' + req.body.username + ' already exists!');
-      err.status = 403;
-      next(err);
+  User.register(new User({username: req.body.username}), 
+    req.body.password, (err, user) => {
+    if(err) {
+      res.statusCode = 500;
+      res.setHeader('Context-Type', 'application/json');
+      res.json({ err: err});
     } 
     else {
-      return User.create({
-        username: req.body.username,
-        password: req.body.password});
+      passport.authenticate('local')(req, res, () => {
+        res.statusCode = 200;
+        res.setHeader('Context-Type', 'application/json');
+        res.json({success: true, status: 'Registration Successful!'});
+      });
     }
-  })
-  .then((user) => {
-    res.statusCode = 200;
-    res.setHeader('Context-Type', 'application/json');
-    res.json({status: 'Registration Successful!', user: user});
-  }, (err) => next(err))
-  .catch((err) => next(err));
+  });
 });
 
 // authentication
 // post because need to submit information
-router.post('/login', (req, res, next) => {
-  if (!req.session.user) {
-    var authHeader = req.headers.authorization;
+router.post('/login', passport.authenticate('local'), (req, res) => {
 
-    if (!authHeader) {
-      var err = new Error('You are not authenticated!');
-
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
-
-    // authorization middleware
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
-
-    var username = auth[0];
-    var password = auth[1];
-
-    User.findOne({username: username})
-    .then((user) => {
-      if (user === null) {
-        var err = new Error('User ' + username + ' does not exist!');
-        err.status = 403;
-        return next(err); 
-      }
-      else if (user.password !== password) {
-        var err = new Error('Your password is incorrect!');
-        err.status = 403;
-        return next(err); 
-      }
-      else if (user.username === username && user.password === password) {
-        req.session.user = 'authenticated';
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('You are authenticated!');
-      }
-    })
-    .catch((err) => next(err));
-  }
-  else {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('You are already authenticated!');
-  }
+  var token = authenticate.getToken({_id: req.user._id})
+  res.statusCode = 200;
+  res.setHeader('Context-Type', 'application/json');
+  res.json({success: true, token: token, status: 'You are successfully logged in!'});
 });
 
 // use only get because no need to send any fundamental info
